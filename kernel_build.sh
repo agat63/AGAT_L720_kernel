9bb00511a4a7723165b1f56825466c32ad243d05
Talustus <talustus@dream-irc.de>
2013-05-25 04:42:07 +0200

    OC/UC Build 2   * enable Low CPU Clocks   * enable CONSERVATIVE CPU Gov

--- kernel_build.sh
+++ kernel_build.sh
@@ -51,7 +51,7 @@ export KBUILD_BUILD_HOST=`hostname | sed 's|ip-projects.de|dream-irc.com|g'`
 # Version of this Build
 #
 ## 1.0 for initial build
-KRNRLS="DreamKernel-GT-I9505-v1.0.1DBG6_OC"
+KRNRLS="DreamKernel-GT-I9505-v1.0.1DBG6_OC2"
 
 
 #
