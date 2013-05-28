83b56fd885dd0d50059bf0ae3855ab6812a95b09
Talustus <talustus@dream-irc.de>
2013-05-28 12:57:18 +0200

    Revert "msm_fb: display: add mutex to video_off"
    This reverts commit 683ff8acdbd27d1d2431f1e1e0c489608404d719.

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -220,11 +220,6 @@ int mdp4_dtv_pipe_commit(int cndx, int wait)
 	undx =  vctrl->update_ndx;
 	vp = &vctrl->vlist[undx];
 	pipe = vctrl->base_pipe;
-	if (pipe == NULL) {
-		pr_err("%s: NO base pipe\n", __func__);
-		mutex_unlock(&vctrl->update_lock);
-		return 0;
-	}
 	mixer = pipe->mixer_num;
 
         mdp_update_pm(vctrl->mfd, vctrl->vsync_time);
@@ -665,8 +660,6 @@ int mdp4_dtv_on(struct platform_device *pdev)
 	if (mfd->key != MFD_KEY)
 		return -EINVAL;
 
-	mutex_lock(&mfd->dma->ov_mutex);
-
 	vctrl->dev = mfd->fbi->dev;
 	vctrl->vsync_irq_enabled = 0;
 
@@ -681,7 +674,6 @@ int mdp4_dtv_on(struct platform_device *pdev)
 		if (mdp4_overlay_dtv_set(mfd, NULL)) {
 			pr_warn("%s: dtv_pipe is NULL, dtv_set failed\n",
 				__func__);
-			mutex_unlock(&mfd->dma->ov_mutex);
 			return -EINVAL;
 		}
 	}
@@ -691,8 +683,6 @@ int mdp4_dtv_on(struct platform_device *pdev)
 		pr_warn("%s: panel_next_on failed", __func__);
 
 	atomic_set(&vctrl->suspend, 0);
-
-	mutex_unlock(&mfd->dma->ov_mutex);
 
 	pr_info("%s:\n", __func__);
 
@@ -724,8 +714,6 @@ int mdp4_dtv_off(struct platform_device *pdev)
 	struct vsync_update *vp;
 
 	mfd = (struct msm_fb_data_type *)platform_get_drvdata(pdev);
-
-	mutex_lock(&mfd->dma->ov_mutex);
 
 	vctrl = &vsync_ctrl_db[cndx];
 
@@ -776,8 +764,6 @@ int mdp4_dtv_off(struct platform_device *pdev)
 
 	/* Mdp clock disable */
 	mdp_clk_ctrl(0);
-
-	mutex_unlock(&mfd->dma->ov_mutex);
 
 	pr_info("%s:\n", __func__);
 	return ret;
